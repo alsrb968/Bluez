a11321d3087288bcb1467664c33285866c41a73d
alsrb968 <alsrb968@naver.com>
2017-01-31 21:24:51 +0900

    Update myblescan.c

--- bluez-master/myblescan.c
+++ bluez-master/myblescan.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/hci_lib.h>
+#include <bluez-master/bluetooth.h>
+#include <bluez-master/hci.h>
+#include <bluez-master/hci_lib.h>
 
 int main(int argc, char **argv)
 {
